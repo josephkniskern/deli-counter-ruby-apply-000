50a2a5d11b9c9522260adfa3f5331965248fec82
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-07 21:39:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,19 @@
 katz_deli = []
 
 def line(katz_deli)
-  line_currently = "The line is currently:"
+  line_currently = "The line is currently:"  
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, idx|
-      line_currently += " #{idx + 1}. #{name}"
+      line_currently += " #{idx + 1}. #{name}"  
     end
     puts "#{line_currently}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli << name # also use .unshift(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
@@ -21,10 +21,17 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+    puts "Currently serving #{katz_deli[0]}." # also katz_deli.first 
+    katz_deli.shift  # .delete.first
   end
 end
+
+number = 0
+
+def take_a_number_two(number)
+  return number += 1
+end
+  
 
 
     
